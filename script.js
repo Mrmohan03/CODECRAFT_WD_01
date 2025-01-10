f272f44b79be55de41a32b7b4f4542f0257168eb
Mohan Lobhaji Yedatkar <your-email@example.com>
2025-01-10 12:56:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add a class when scrolling to change navbar background
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
